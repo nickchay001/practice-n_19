592906f1e6b4d4cbabed017e1a0c763ec7a5aac6
unknown <nchaygta5@gmail.com>
2023-01-02 13:54:13 +0200

    header finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu){
+	const headerMenu = document.querySelector('.header__menu');
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		headerMenu.classList.toggle('_active');
+	})
+}
+
